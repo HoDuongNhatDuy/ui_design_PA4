b9eb46c7e3393e180862bc4106cc540f6700e36f
Jake <forte2610@hotmail.com>
2018-05-11 22:11:27 +0700

    Cập nhật phần upload hình ảnh thú nuôi

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-
-
-$( document ).ready(function() { 
+$(document).ready(function () {
 
 	// var titlepost='';
 
@@ -14,149 +12,153 @@ $( document ).ready(function() {
 
 	// var pricepost=0;
 
-     $('#upload-file-pet').on('change',function (event)
-        {
-			if($('.add-image-pet').hasClass('none-image-choose'))
-				$('.add-image-pet').removeClass('none-image-choose');
-            //var filename = $('#upload-file-pet').val().split('\\').pop();
-	        var file = document.getElementById('upload-file-pet').files[0];
-	        var reader  = new FileReader();
-	        // it's onload event and you forgot (parameters)
-	        reader.onload = function(e)  {
-		        var path = e.target.result;
-		        var divimg = `
+	$('#upload-file-pet').on('change', function (event) {
+		if ($('.add-image-pet').hasClass('none-image-choose'))
+			$('.add-image-pet').removeClass('none-image-choose');
+		//var filename = $('#upload-file-pet').val().split('\\').pop();
+		var file = document.getElementById('upload-file-pet').files[0];
+		var reader = new FileReader();
+		// it's onload event and you forgot (parameters)
+		reader.onload = function (e) {
+			var path = e.target.result;
+			var divimg = `
 							<div class="image-pet">
-		                        <img src="`+ path +`" alt="no image" class="set-image">
-		                        <i class="fas fa-times remove-image-pet"></i>
-		                    </div>
+                        <img src="` + path + `" alt="no image" class="set-image">
+                        <div class="remove-image-pet"></div>
+                    </div>
             			`;
 
-		        $('.div-choose-image-pet').append(divimg);
-		        checkimage();
-	        }
-	        // you have to declare the file loading
-	        reader.readAsDataURL(file);
+			$('.div-choose-image-pet').append(divimg);
+			checkimage();
+		}
+		// you have to declare the file loading
+		reader.readAsDataURL(file);
 
-            //alert(count_img);
-        });
-    $('.add-image-pet').on('click',function(){
-    	$('#upload-file-pet').trigger('click');
+		//alert(count_img);
+	});
+	$('.add-image-pet').on('click', function () {
+		$('#upload-file-pet').trigger('click');
 
-     });
-	$('.add-criterion').click(function() {
+	});
+	$('.add-criterion').click(function () {
 		$('#criteria-modal').modal();
 	});
 
-    $('a.step-submit-1').on('click',function(){
-    	var check1 = checknamepost();
-    	var check2 = checknamepet();
-    	console.log('aaaa');
-    	if(check1 !== '' && check2 !== '') {
-    		window.location.href = 'find-owner-2.html';
-    	}
-    });
-	$('a.step-submit-2').on('click',function(){
+	$('a.step-submit-1').on('click', function () {
+		var check1 = checknamepost();
+		var check2 = checknamepet();
+		console.log('aaaa');
+		if (check1 !== '' && check2 !== '') {
+			window.location.href = 'find-owner-2.html';
+		}
+	});
+	$('a.step-submit-2').on('click', function () {
 		var checkimg = checkimage();
-		if(checkimg !== 0) {
+		if (checkimg !== 0) {
 			window.location.href = 'find-owner-3.html';
-		}		
-    });
-    $('a.step-submit-3').on('click',function(){
+		}
+	});
+	$('a.step-submit-3').on('click', function () {
 
-    	window.location.href = 'pets-find-owner.html';
-    });
+		window.location.href = 'pets-find-owner.html';
+	});
 
-	$('.namepost #text-namepost').on('blur',function(){
+	$('.namepost #text-namepost').on('blur', function () {
 		checknamepost();
 	});
-	$('.namepet #text-namepet').on('blur',function(){
+	$('.namepet #text-namepet').on('blur', function () {
 		checknamepet();
 	});
-	function checkimage(){
+
+	function checkimage() {
 		var countimg = $('.div-choose-image-pet .image-pet').length;
-		if(countimg === 0){
+		if (countimg === 0) {
 			$('#mute-imagepet').text('Phải có ít nhất 1 hình ảnh');
-		}
-		else $('#mute-imagepet').text('');
+		} else $('#mute-imagepet').text('');
 
 		return countimg;
 	}
-	function checknamepost(){
-		var thongbao='';
-    	var tenpost = $('.namepost #text-namepost').val();
-    	if(tenpost === '' ) thongbao = 'Tên bài đăng không được trống';
-    	$('.namepost #mute-namepost').text(thongbao);
-    	return tenpost;
-	}
-	function checknamepet(){
-		var thongbao='';
-    	var tenpet = $('.namepet #text-namepet').val();
-    	if(tenpet === '' ) thongbao = 'Tên thú cưng không được trống';
-    	$('.namepet #mute-namepet').text(thongbao);
-    	return tenpet;
+
+	function checknamepost() {
+		var thongbao = '';
+		var tenpost = $('.namepost #text-namepost').val();
+		if (tenpost === '') thongbao = 'Tên bài đăng không được trống';
+		$('.namepost #mute-namepost').text(thongbao);
+		return tenpost;
 	}
 
-	$('.list-feature-pet').on('click', '.remove-feature', function(){
+	function checknamepet() {
+		var thongbao = '';
+		var tenpet = $('.namepet #text-namepet').val();
+		if (tenpet === '') thongbao = 'Tên thú cưng không được trống';
+		$('.namepet #mute-namepet').text(thongbao);
+		return tenpet;
+	}
+
+	$('.list-feature-pet').on('click', '.remove-feature', function () {
 		$(this).closest('tr').remove();
 	});
-	$('.div-choose-image-pet').on('click', '.remove-image-pet', function(){
-		$(this).closest('div').remove();
+	$('.div-choose-image-pet').on('click', '.remove-image-pet', function () {
+		$(this).closest(".image-pet").remove();
 		addclassnoneimage();
 	});
-	function addclassnoneimage(){
-		if($('.div-choose-image-pet .image-pet').length === 0){
-			if(!$('.add-image-pet').hasClass('none-image-choose'))
-				$('.add-image-pet').addClass('none-image-choose');
-		}else{
-			if($('.add-image-pet').hasClass('none-image-choose'))
-				$('.add-image-pet').removeClass('none-image-choose');
+
+	function addclassnoneimage() {
+		if ($('.div-choose-image-pet .image-pet').length === 0) {
+			if (!$('.btn .add-image-pet').hasClass('none-image-choose'))
+				$('.btn .add-image-pet').addClass('none-image-choose');
+		} else {
+			if ($('.btn .add-image-pet').hasClass('none-image-choose'))
+				$('.btn .add-image-pet').removeClass('none-image-choose');
 		}
 	}
-	$('#price-find-owner').on('keypress',function(e){
-	    
-	      if(e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57))
-	      {
-	        return false;
-	      }
-	 });
+	$('#price-find-owner').on('keypress', function (e) {
 
-	$('#price-find-owner').on('keyup',function(){
+		if (e.which != 8 && e.which != 0 && (e.which < 48 || e.which > 57)) {
+			return false;
+		}
+	});
+
+	$('#price-find-owner').on('keyup', function () {
 		var price = $('#price-find-owner').val();
-		if(price === '')return;
+		if (price === '') return;
 		price = price.toString();
 		price = getMoney(price);
 
 		var price_fm = formatMoney(price);
 		$('#price-find-owner').val(price_fm);
 	});
-	$('#price-find-owner').on('blur',function(){
+	$('#price-find-owner').on('blur', function () {
 		var price = $('#price-find-owner').val();
-		if(price === '')$('#price-find-owner').val('0');;
+		if (price === '') $('#price-find-owner').val('0');;
 	});
-	 function getMoney(fm_money){
-	    var fm = fm_money.replace(/\,/g,"");
-	    var money = parseInt(fm);
-	    return money;
-	 }
-	function formatMoney(money){
-	    var strmoney = money.toString();
-	
-	    var l = strmoney.length;
-	    var sodayphay = Math.ceil(l/3) - 1;
-	    for(var i = 1;i<= sodayphay; i++){
-	      strmoney = insert(strmoney,l-i*3,',');
-	    }
-	    return strmoney;
+
+	function getMoney(fm_money) {
+		var fm = fm_money.replace(/\,/g, "");
+		var money = parseInt(fm);
+		return money;
 	}
+
+	function formatMoney(money) {
+		var strmoney = money.toString();
+
+		var l = strmoney.length;
+		var sodayphay = Math.ceil(l / 3) - 1;
+		for (var i = 1; i <= sodayphay; i++) {
+			strmoney = insert(strmoney, l - i * 3, ',');
+		}
+		return strmoney;
+	}
+
 	function insert(str, index, value) {
-	    return str.substr(0, index) + value + str.substr(index);
+		return str.substr(0, index) + value + str.substr(index);
 	}
 
-	$('.find-owner-submit-new-post').on('click', function(){
-		window.location.href='find-owner-1.html';
+	$('.find-owner-submit-new-post').on('click', function () {
+		window.location.href = 'find-owner-1.html';
 	});
 
-	$('.re-step-submit-2,.re-step-submit-3').on('click', function(){
+	$('.re-step-submit-2,.re-step-submit-3').on('click', function () {
 		window.history.back();
 	});
 
